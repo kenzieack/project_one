1b21d0f74b6a46887dfa5e2442917eedae660d14
Ingrid N <inorrmann@gmail.com>
2019-10-31 08:37:06 -0700

    dropdown menus id changed for iTunes API

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 $('.dropdown-trigger').dropdown();
 
 
+// iTunes API
 var queryUrl = "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=hip-hop/rap&media=music&entity=musicTrack"
+
+// var queryUrl = "https://itunes.apple.com/search?term=hip-hop/rap&media=music&entity=musicTrack"
+
+var media = "";
+var term = "";
+var entity = "";
+var search = term + "&" + media + "&" + entity
 
 // "https://itunes.apple.com/search?key1=value1&key2=value2&key3=value3"
 
 
 
-// connecting AJAX request 
+// connecting AJAX request to iTunes API
 $.ajax({
   url: queryUrl,
   method: "GET"
@@ -20,3 +28,14 @@ $.ajax({
   console.log(topValues[1][2].artistName);
 
 });
+
+
+// on-click event for media dropdown menu
+$("#dropdown-media").on("click", function () {
+  media = "media=" + event.target.id;
+
+  console.log(media);
+  console.log(search);
+
+
+}) // end of on-click event for media dropdown menu
