e4e6f327e43ac416b976039266af03f96da09ce7
Ingrid Norrmann <55808113+inorrmann@users.noreply.github.com>
2019-10-30 19:03:39 -0700

    Update script.js
    iTunes API was added

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 // initializing dropdown
 $('.dropdown-trigger').dropdown();
 
+
+var queryUrl = "https://cors-anywhere.herokuapp.com/https://itunes.apple.com/search?term=hip-hop/rap&media=music&entity=musicTrack"
+
+// "https://itunes.apple.com/search?key1=value1&key2=value2&key3=value3"
+
+
+
+// connecting AJAX request 
+$.ajax({
+  url: queryUrl,
+  method: "GET"
+}).then(function (response) {
+
+  var topResults = JSON.parse(response);
+  console.log(topResults);
+  var topValues = Object.values(topResults);
+  console.log(topValues[1][2].artistName);
+
+});
